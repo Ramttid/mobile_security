cd0cb6d8c0b19b75d887e95f1d9ed1097ad86674
Braet Dittmar <dittmar.braet@student.howest.be>
2024-05-07 01:42:43 +0200

    Update MainActivity

--- app/src/main/java/com/example/mobile_security_project/MainActivity.kt
+++ app/src/main/java/com/example/mobile_security_project/MainActivity.kt
@@ -3,57 +3,58 @@ package com.example.mobile_security_project
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.mobile_security_project.ui.theme.MobilesecurityprojectTheme
+import com.example.mobile_security_project.ui.interfaces.LoginForm
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MobilesecurityprojectTheme {
-                // A surface container using the 'background' color from the theme
-                Greeting("Android")
-            }
+//            val  = rememberNavController()
+//            NavHost(navController = navController, startDestination = "login_screen", builder = {
+//                composable("login_screen",){
+//                    LoginForm(navController)
+//                }
+//                composable("chucknorris_screen",){
+//                    ChuckNorrisForm()
+//                }
+//            })
+            LoginForm()
+           // LoginForm()
+//            MobilesecurityprojectTheme {
+//                Greeting("Android")
+//            }
         }
     }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface {
-        Column(
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.fillMaxSize()
-        ) {
-            Text(
-                text = "Hello $name!",
-                modifier = modifier
-            )
-        }
-    }
-}
-
-@Preview(showBackground = true, showSystemUi = true)
-@Composable
-fun GreetingPreview() {
-    MobilesecurityprojectTheme {
-        Greeting("Android")
-    }
-}
-
-@Preview(showBackground = true, showSystemUi = true)
-@Composable
-fun GreetingPreviewDark() {
-    MobilesecurityprojectTheme(darkTheme = true) {
-        Greeting("Android")
-    }
-}
+//
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Surface {
+//        Column(
+//            verticalArrangement = Arrangement.Center,
+//            horizontalAlignment = Alignment.CenterHorizontally,
+//            modifier = Modifier.fillMaxSize()
+//        ) {
+//            Text(
+//                text = "Hello $name!",
+//                modifier = modifier
+//            )
+//        }
+//    }
+//}
+//
+//@Preview(showBackground = true, showSystemUi = true)
+//@Composable
+//fun GreetingPreview() {
+//    MobilesecurityprojectTheme {
+//        Greeting("Android")
+//    }
+//}
+//
+//@Preview(showBackground = true, showSystemUi = true)
+//@Composable
+//fun GreetingPreviewDark() {
+//    MobilesecurityprojectTheme(darkTheme = true) {
+//        Greeting("Android")
+//    }
+//}
